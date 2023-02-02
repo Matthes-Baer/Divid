2e103206a9bd5bc9095b6c282bf1e69d95dd7be7
Matthes-Baer <Matthes.baer@yahoo.de>
2023-02-03 00:29:10 +0100

    feat: Added some notes

---
+++ screens/Start.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, View, Text } from "react-native";
+
+export default function Start(): JSX.Element {
+  return (
+    <View>
+      <Text>Eigene Font aktivieren</Text>
+      <Text>
+        Authentication mit Firebase einrichten - https://rnfirebase.io/
+      </Text>
+      <Text>
+        Database-Funktionen mit Firebase einrichten, die als authenticated user
+        genutzt werden können - https://rnfirebase.io/database/usage
+      </Text>
+      <Text>Routing, ...</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
